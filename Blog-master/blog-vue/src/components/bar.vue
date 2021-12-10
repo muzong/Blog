0b10b282937b0bf9d347aa484fc880635d936c76
@liusy <1406618174@qq.com>
2021-12-10 17:06:18 +0800

    黄浩笨蛋

--- Blog-master/blog-vue/src/components/bar.vue
+++ Blog-master/blog-vue/src/components/bar.vue
@@ -6,109 +6,58 @@
                  @select="handleSelect">
 
 
-          <el-menu-item index="1" route="/">首页</el-menu-item>
-          <el-menu-item index="2" style="margin-left: 2%" route="/message">留言板</el-menu-item>
-          <el-menu-item index="3" style="margin-left: 2%" route="/announcement">本站公告</el-menu-item>
+<!--          <el-menu-item index="1" route="/">首页</el-menu-item>-->
+<!--          <el-menu-item index="2" style="margin-left: 2%" route="/message">留言板</el-menu-item>-->
+<!--          <el-menu-item index="3" style="margin-left: 2%" route="/announcement">本站公告</el-menu-item>-->
 
 
-          <el-menu-item id="space" index="" @click="egg" class="hidden-xs-only"/>
-          <!--<el-menu-item/>-->
-
+<!--          <el-menu-item id="space" index="" @click="egg" class="hidden-xs-only"/>-->
+<!--          &lt;!&ndash;<el-menu-item/>&ndash;&gt;-->
 
           <div style="width: 20%;float: left;margin: 10px 2% 0px -5%" class="hidden-xs-only">
-            <el-input placeholder="搜索博客" v-model="searchTxt" suffix-icon="el-icon-search"
+            <el-input style="left: 30%" placeholder="搜索博客" v-if="showSearch" v-model="searchTxt" suffix-icon="el-icon-search"
                       @keyup.enter.native="searchSubmit"/>
           </div>
 
+          <el-menu-item style="left: 30%">
+            <span>
+              <router-link to="/" style="text-decoration: none;font-size: 25px">博客客客客</router-link>
+            </span>
+          </el-menu-item>
+          <el-menu-item class="hidden-xs-only"  style="left: 60%;  ">
+            <el-button v-if="this.$store.state.token==''" type="text" @click="toLogin">登录</el-button>
 
-          <el-menu-item class="hidden-xs-only" v-if="this.$store.state.token==''" index=""
-                        @click="loginFormVisible = true">
-            <el-button type="text">登录</el-button>
+            <el-submenu class="hidden-xs-only" v-if="this.$store.state.token!==''" :router="true" index="4" style="width:120px">
+              <template  slot="title">&nbsp;&nbsp;{{this.$store.state.name}}&nbsp;&nbsp;</template>
+
+              <el-menu-item route="/newBlog" index="4-1">&nbsp;&nbsp;&nbsp;
+                <i class="el-icon-edit"></i>
+                写博客
+              </el-menu-item>
+              <el-menu-item route="/myBlog" index="4-3">&nbsp;&nbsp;&nbsp;
+                <i class="el-icon-s-home"></i>
+                我的博客
+              </el-menu-item>
+              <el-menu-item route="/account" index="4-2">&nbsp;&nbsp;&nbsp;
+                <i class="el-icon-s-tools"></i>
+                账号设置
+              </el-menu-item>
+
+
+              <el-menu-item route="/admins/userManage" index="4-4" v-if="this.$store.state.roles.indexOf('ADMIN') > -1">
+                &nbsp;&nbsp;&nbsp;
+                <i class="el-icon-loading"></i>管理后台
+              </el-menu-item>
+
+              <el-menu-item @click="logout">&nbsp;&nbsp;&nbsp;
+                <i class="el-icon-switch-button"/>退出登录
+              </el-menu-item>
+            </el-submenu>
           </el-menu-item>
 
-          <el-menu-item class="hidden-xs-only" v-if="this.$store.state.token==''" index=""
-                        @click="registerFormVisible = true">
-            <el-button type="text">注册</el-button>
+          <el-menu-item class="hidden-xs-only" v-if="this.$store.state.token==''" style="left: 60%">
+            <el-button type="text" @click="toRegister">注册</el-button>
           </el-menu-item>
-
-          <el-dialog title="登录" :visible.sync="loginFormVisible" width="21%">
-            <el-form :model="form">
-              <el-form-item :label-width="formLabelWidth">
-                <el-input v-model="form.loginName" placeholder="用户名" prefixIcon="el-icon-user-solid"/>
-              </el-form-item>
-              <el-form-item :label-width="formLabelWidth">
-                <el-input v-model="form.loginPwd" placeholder="密码" :show-password="form.loginShowPwd"
-                          @keyup.enter.native="userLogin" prefixIcon="el-icon-lock"/>
-              </el-form-item>
-              <el-button @click="userLogin">登录&nbsp;&nbsp;<i class="el-icon-check"></i></el-button>
-              <router-link to="/forgetPwd">
-                <el-button @click="loginFormVisible = false">忘记密码&nbsp;&nbsp;<i class="el-icon-right"></i></el-button>
-              </router-link>
-            </el-form>
-          </el-dialog>
-
-          <el-dialog title="注册" :visible.sync="registerFormVisible" width="30%">
-
-            <el-form :model="form">
-              <el-form-item :label-width="formLabelWidth">
-                <el-input v-model="form.registerName" placeholder="用户名" prefixIcon="el-icon-user-solid"/>
-              </el-form-item>
-              <el-form-item :label-width="formLabelWidth">
-                <el-input v-model="form.registerPwd" placeholder="密码" :show-password="form.loginShowPwd"
-                          prefixIcon="el-icon-lock"/>
-              </el-form-item>
-
-              <el-form-item :label-width="formLabelWidth">
-                <el-input v-model="form.registerConfirmPwd" placeholder="确认密码" :show-password="form.loginShowPwd"
-                          prefixIcon="el-icon-bell"/>
-              </el-form-item>
-
-
-              <el-form-item :label-width="formLabelWidth">
-                <el-input v-model="form.registerInviteCode" placeholder="邀请码" prefixIcon="el-icon-s-promotion"/>
-              </el-form-item>
-              <el-form-item :label-width="formLabelWidth">
-                <el-input v-model="form.registerMail" placeholder="邮箱" prefixIcon="el-icon-message"/>
-              </el-form-item>
-              <el-form-item :label-width="formLabelWidth">
-                <el-input v-model="form.registerMailCode" placeholder="邮箱验证码" prefixIcon="el-icon-key"/>
-              </el-form-item>
-              <el-button @click="sendMail">
-                发送验证码&nbsp;&nbsp;
-                <i class="el-icon-coffee-cup" v-if="!sendMailFlag"/>
-                <i class="el-icon-loading" v-if="sendMailFlag"/>
-              </el-button>
-              <el-button @click="userRegister">注册&nbsp;&nbsp;<i class="el-icon-check"></i></el-button>
-            </el-form>
-          </el-dialog>
-
-
-          <el-submenu class="hidden-xs-only" index="4" v-if="this.$store.state.token!==''" :router="true">
-            <template slot="title">[&nbsp;&nbsp;{{this.$store.state.name}}&nbsp;&nbsp;]</template>
-
-            <el-menu-item route="/newBlog" index="4-1">&nbsp;&nbsp;&nbsp;
-              <i class="el-icon-edit"></i>
-              写博客
-            </el-menu-item>
-            <el-menu-item route="/myBlog" index="4-3">&nbsp;&nbsp;&nbsp;
-              <i class="el-icon-s-home"></i>
-              我的博客
-            </el-menu-item>
-            <el-menu-item route="/account" index="4-2">&nbsp;&nbsp;&nbsp;
-              <i class="el-icon-s-tools"></i>
-              账号设置
-            </el-menu-item>
-
-
-            <el-menu-item route="/admins/userManage" index="4-4" v-if="this.$store.state.roles.indexOf('ADMIN') > -1">
-              &nbsp;&nbsp;&nbsp;
-              <i class="el-icon-loading"></i>管理后台
-            </el-menu-item>
-
-            <el-menu-item @click="logout">&nbsp;&nbsp;&nbsp;
-              <i class="el-icon-switch-button"/>退出登录
-            </el-menu-item>
-          </el-submenu>
 
         </el-menu>
       </el-row>
@@ -128,21 +77,7 @@
       return {
         searchTxt: '',//搜索框
         activeIndex: '1',//默认选中第一项->首页
-        loginFormVisible: false,//登录框
-        registerFormVisible: false,//注册框
-        form: { //表单
-          loginName: '',
-          loginPwd: '',
-          loginShowPwd: true, //登录是否显示密码
-          registerName: '',//注册
-          registerPwd: '',
-          registerConfirmPwd: '',
-          registerMail: '',
-          registerMailCode: '',
-          registerInviteCode: ''
-        },
-        formLabelWidth: '0px',
-        sendMailFlag: false
+        showSearch:true,
       }
     },
     watch: {
@@ -165,11 +100,19 @@
       }
     },
     methods: {
+      toLogin() {
+        this.$router.push({path: '/login'})
+        this.showSearch = false
+      },
+      toRegister(){
+        this.$router.push({path: '/register'})
+      },
       handleSelect(key, keyPath) {
 //        console.log(key, keyPath);
         if (key != null && key !== '')
           this.activeIndex = key
       },
+
       egg() {
         this.$notify({
           title: '彩蛋',
@@ -177,24 +120,6 @@
           type: 'success',
           duration: 120
         });
-      },
-      userLogin() { //登录方法
-        if (this.form.loginName.length <= 0 || this.form.loginPwd.length <= 0) {
-          this.$message({
-            message: '字段不完整',
-            type: 'error'
-          });
-          return;
-        }
-        user.login(this.form.loginName, this.form.loginPwd).then(response => {
-          this.form.loginPwd = ''
-          this.$store.commit('login', response.data)//存储token
-          this.$message({
-            message: '登录成功',
-            type: 'success'
-          });
-          this.loginFormVisible = false
-        })
       },
       logout() {  //退出登录
         user.logout().then(res => {
@@ -206,85 +131,6 @@
           this.$router.push({ //路由跳转
             path: '/'
           })
-        })
-
-      },
-      sendMail() {//发送邮件
-
-        const reg = new RegExp(/^([a-zA-Z0-9._-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/);
-        if (!reg.test(this.form.registerMail)) {//检测字符串是否符合正则表达式
-          this.$message({
-            message: '邮箱格式不正确',
-            type: 'error'
-          });
-          return;
-        }
-        this.sendMailFlag = true;
-        const mail = this.form.registerMail;
-        const _this = this;
-        user.sendMail(mail).then(res => {
-          //成功调用接口后设置定时器，每隔600ms查询一次邮件状态
-          var intervalId = setInterval(function () {
-            user.getMailSendState(mail).then(res => {
-              //邮件状态发生改变时
-              if (res.data !== '0') {
-                //清除定时器
-                clearInterval(intervalId);
-                if (res.data === '1') {
-                  _this.$message.success('发送成功');
-                } else {
-                  _this.$message.error('发送失败');
-                }
-                _this.sendMailFlag = false;
-              }
-            });
-          }, 600);
-
-        }).catch(() => {
-          this.sendMailFlag = false;
-          this.$message.error('发送失败');
-        })
-      },
-      userRegister() {//用户注册
-        //判断是否输入字段
-        if (this.form.registerName.length <= 0 || this.form.registerPwd.length <= 0 || this.form.registerConfirmPwd.length <= 0 ||
-          this.form.registerMail.length <= 0 || this.form.registerMailCode.length <= 0 || this.form.registerInviteCode.length <= 0) {
-          this.$message({
-            message: '字段不符合规范',
-            type: 'error'
-          });
-          return;
-        }
-        if (this.form.registerPwd !== this.form.registerConfirmPwd) {
-          this.$message({
-            message: '两次密码不一致',
-            type: 'error'
-          });
-          return;
-        }
-        if (this.form.registerPwd.length < 6) {
-          this.$message({
-            message: '密码长度需大于6位',
-            type: 'error'
-          });
-          return;
-        }
-        var reg = new RegExp(/^([a-zA-Z0-9._-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/);
-
-        if (!reg.test(this.form.registerMail)) {//检测字符串是否符合正则表达式
-          this.$message({
-            message: '邮箱格式不正确',
-            type: 'error'
-          });
-          return;
-        }
-
-        user.register(this.form.registerName, this.form.registerPwd, this.form.registerMail, this.form.registerMailCode, this.form.registerInviteCode).then(res => {
-          this.$message({
-            message: '注册成功',
-            type: 'success'
-          })
-          this.registerFormVisible = false
         })
 
       },
